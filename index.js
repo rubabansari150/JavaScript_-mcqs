7c538c9314460a0e9693b8a481d7735839bea29f
rubabansari150 <rubabansari21992@gmail.com>
2024-08-15 23:28:54 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+// 01
+var mcqs_1 = prompt(" What is the correct syntax for an if statement in JavaScript?");
+if(mcqs_1 === "if (condition) {  }"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 02
+var mcqs_2 = prompt("How do you write an if statement to execute some code if i is equal to 5?");
+if(mcqs_2 === "if (i == 5) { }"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 03
+var mcqs_3 = prompt("Where is the correct place to insert a JavaScript?");
+if(mcqs_3 === "The body section"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 04
+var mcqs_4 = prompt("The external JavaScript file must contain the <script> tag.");
+if(mcqs_4 === "true"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 05
+var mcqs_5 = prompt("Choose the correct HTML element for the largest heading:");
+if(mcqs_5 === "h1"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 06
+var mcqs_6 = prompt("What is the correct HTML element for inserting a line break?");
+if(mcqs_6 === "br"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 07
+var mcqs_7 = prompt("Choose the correct HTML element to define important text");
+if(mcqs_7 === "strong"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 08
+var mcqs_8 = prompt("Choose the correct HTML element to define emphasized text");
+if(mcqs_8 === "em"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 09
+var mcqs_9 = prompt("Which of these elements are all table elements?");
+if(mcqs_9 === "table tr td"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
+// 10
+var mcqs_10 = prompt("How can you make a numbered list?");
+if(mcqs_10 === "ol"){
+    alert("Correct Answer");
+}else{
+    alert("Wrong Answer");
+}
